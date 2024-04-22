20b866c01a9f8dbfdc397f1e884776011e0a5b3d
anasdev92 <elyamni.anas@gmail.com>
2024-04-22 16:12:21 -0700

    third commit

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,28 +1,14 @@
 
 #!/usr/bin/env python3
-
-
-"""
-module with a fuction to compute the
-average excetion time for running a subroutine
-"""
-
-from asyncio import run
-from time import time
+"""Measures the total execution time for wait_n(n, max_delay)"""
+import asyncio
+import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    """
-    Measure the excetution time of a subroutine
-    Args:
-        n (int): number of times to run the subroutine
-        max_delay (int): maximum amout of time to delay
-    Returns:
-        returns a float
-    """
-    if isinstance(n, int) and isinstance(max_delay, int):
-        c_time: float = time()
-        run(wait_n(n, max_delay))
-        return (time() - c_time) / n
-    raise TypeError('expected integer values to be passed!')
+    """Returns total_time (total execution time) / n"""
+    start_time = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.perf_counter()
+    return (end_time - start_time) / n
--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,25 +1,9 @@
-
 #!/usr/bin/env python3
-
-""""
-Module with a function that creates a task
-"""
-
-
-from asyncio import Task, create_task
-
-
+"""Create a task"""
+import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Task:
-    """
-    wait for a random number of some seconds
-    and return an asyncio Task
-    Args:
-        max_delay (int): maximum number of seconds
-        to wait
-    Returns:
-        returns an asyncio task
-    """
-    return create_task(wait_random(max_delay))
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """Return a task"""
+    return asyncio.create_task(wait_random(max_delay))
