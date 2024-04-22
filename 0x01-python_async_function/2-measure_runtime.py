3870c3b2fa880657fd5ab9879c7e25d567c8987f
anasdev92 <elyamni.anas@gmail.com>
2024-04-22 14:10:24 -0700

    0x01-python_async_function

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,26 +1,28 @@
 
 #!/usr/bin/env python3
-"""
-Defines runtime of an asynchronous function
-"""
-import asyncio.run(wait_n)
-import time
-import asyncio
 
+
+"""
+module with a fuction to compute the
+average excetion time for running a subroutine
+"""
+
+from asyncio import run
+from time import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
     """
-    Takes two integers and returns the time it takes
-    for the function to execute
+    Measure the excetution time of a subroutine
     Args:
-        int (int)
-        max_delay (int)
+        n (int): number of times to run the subroutine
+        max_delay (int): maximum amout of time to delay
     Returns:
-        float
+        returns a float
     """
-    start: float = time.time()
-    asyncio.run(wait_n(n, max_delay))
-    end: float = time.time()
-    return (end - start)
+    if isinstance(n, int) and isinstance(max_delay, int):
+        c_time: float = time()
+        run(wait_n(n, max_delay))
+        return (time() - c_time) / n
+    raise TypeError('expected integer values to be passed!')
