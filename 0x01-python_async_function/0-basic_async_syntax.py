20b0a6637d5d06a648911bc29483cd073ff525fc
anasdev92 <elyamni.anas@gmail.com>
2024-04-22 16:34:58 -0700

    third commit

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,21 +1,15 @@
 
 #!/usr/bin/env python3
-"""
-Defines a basic asynchronous routine
-"""
+"""Execute multiple coroutines at the same time with async"""
+from typing import List
 import asyncio
-import random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_random(max_delay: int = 10) -> float:
-    """
-    An asynchronous coroutine that waits for a random delay
-    between 0 and max_delay (included and float value) seconds
-    Args:
-        max_delay (int): nteger argument with a default value of 10
-    Return:
-        (int or float): the random delay time
-    """
-    delay: float = random.uniform(0, max_delay)
-    await asyncio.sleep(delay)
-    return delay
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Spawns wait_random n times with the specified max_delay
+        and returns the list of all the delays (float values)."""
+    futures = [wait_random(max_delay) for _ in range(n)]
+    futures = asyncio.as_completed(futures)
+    delays = [await future for future in futures]
+    return delays
